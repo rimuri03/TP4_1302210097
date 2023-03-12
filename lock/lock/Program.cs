bb5cf2cbeb24c62d36d602b5ea07d3ae18102756
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2023-03-13 00:36:16 +0700

    menambahkan implementasi dengan state-based construction

---
+++ lock/lock/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class DoorMachine
+{
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = new LockedState();
+    }
+
+    public void SetState(State state)
+    {
+        currentState = state;
+        currentState.DisplayState();
+    }
+}
+
+interface State
+{
+    void DisplayState();
+}
+
+class LockedState : State
+{
+    public void DisplayState()
+    {
+        Console.WriteLine("Pintu terkunci");
+    }
+}
+
+class UnlockedState : State
+{
+    public void DisplayState()
+    {
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DoorMachine doorMachine = new DoorMachine();
+
+        // Mengubah state menjadi UnlockedState
+        doorMachine.SetState(new UnlockedState());
+
+        // Mengubah state menjadi LockedState
+        doorMachine.SetState(new LockedState());
+    }
+}
